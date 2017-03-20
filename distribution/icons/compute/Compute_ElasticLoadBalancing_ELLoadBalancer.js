74362cd37afbdb9f87168ad7f99c48d6358ca2f7
Phil Cruz <phil@philcruz.com>
2017-03-20 13:40:56 -0700

    hard-coding viewBox and iconStyles

--- distribution/icons/compute/Compute_ElasticLoadBalancing_ELLoadBalancer.js
+++ distribution/icons/compute/Compute_ElasticLoadBalancing_ELLoadBalancer.js
@@ -50,10 +50,14 @@ var Compute_ElasticLoadBalancing_ELLoadBalancer = function (_React$Component) {
 
 	_createClass(Compute_ElasticLoadBalancing_ELLoadBalancer, [{
 		key: 'render',
-		value: function render() {
+		value: function render(props) {
+			var iconStyles = {
+				height: 100,
+				width: 100
+			};
 			return React.createElement(
 				SvgIcon,
-				{ viewBox: '0 0 100 100' },
+				{ viewBox: '0 0 100 100', style: iconStyles },
 				React.createElement('circle', { cx: '50', cy: '48.75', r: '30.43', fill: '#f58536' }),
 				React.createElement('path', { fill: '#fff', d: 'M24.42 39.87h11.05v17.76H24.42zM59.73 27.74h9.86v9.86h-9.86zM59.73 43.82h9.86v9.86h-9.86zM59.73 59.91h9.86v9.86h-9.86z' }),
 				React.createElement('path', { fill: '#fff', d: 'M57.01 50.05l1.64-.81 1.08-.53-1.08-.54-1.46-.72-4.4-2.17v2.17H39.58l15.01-9.61 1.19 1.84 2.49-4.2.73-1.24.73-1.22-1.4.17-1.59.18-4.7.55 1.13 1.74-18.41 11.79-2.03 1.3 2.42 1.54 18.02 11.55-1.13 1.74 4.7.55 1.59.18 1.4.17-.73-1.22-.73-1.24-2.49-4.2-1.19 1.84-15.01-9.61h13.21v2.09l4.22-2.09' })
